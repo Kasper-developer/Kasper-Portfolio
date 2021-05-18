40b4def914d45785f1c1ffd9fb9b77ed2198b59f
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-05-18 23:23:46 +0500

    create: responsive for all sections + favicon

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 AOS.init()
+
+AOS.init({
+  // Global settings:
+  offset: 90,
+})
+
 const swiper = new Swiper('.swiper-container', {
   speed: 600,
   spaceBetween: 100,
   loop: true,
-	effect: 'coverflow',
+  effect: 'coverflow',
   pagination: {
     el: '.projects-pagination',
     type: 'bullets',
-		bulletClass: 'projects-bullet',
-		bulletActiveClass: 'projects-bullet-active',
-		clickable: true,
+    bulletClass: 'projects-bullet',
+    bulletActiveClass: 'projects-bullet-active',
+    clickable: true,
   },
 })
