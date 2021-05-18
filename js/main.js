c51cbf637f037cd1aeb874357e2db25533499837
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-05-18 16:18:48 +0500

    create: projects sect. + animation + pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-AOS.init();
+AOS.init()
+const swiper = new Swiper('.swiper-container', {
+  speed: 600,
+  spaceBetween: 100,
+  loop: true,
+	effect: 'coverflow',
+  pagination: {
+    el: '.projects-pagination',
+    type: 'bullets',
+		bulletClass: 'projects-bullet',
+		bulletActiveClass: 'projects-bullet-active',
+		clickable: true,
+  },
+})
